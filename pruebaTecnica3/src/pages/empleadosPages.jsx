38faf51357515db711b5be2f2d8054cf2ec37cd7
Sebastian Vallejo Montoya <svallejo2345@gmail.com>
2023-11-06 18:38:04 -0500

    casi completado

--- pruebaTecnica3/src/pages/empleadosPages.jsx
+++ pruebaTecnica3/src/pages/empleadosPages.jsx
@@ -10,19 +10,27 @@ import  {BsGenderAmbiguous} from 'react-icons/bs'
 import {AiOutlineUser} from 'react-icons/ai'
 import '../pages/empleados.css';
 
+
 const Empleados = () => {
-  const { empleados, getEmpleados, createEmpleados, deleteEmpleado  } = useEmpleados();
+  const { empleados, getEmpleados, createEmpleados, deleteEmpleado, updateEmpleado  } = useEmpleados();
   const { roles, getRoles } = useRoles();
   const { areas, getAreas } = useAreas();
   const [rolesSeleccionados, setRolesSeleccionados] = useState([]);
   const { register, handleSubmit } = useForm();
   const [quiereBoletin, setQuiereBoletin] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [empleadoEdit, setEmpleadoEdit ] = useState(null);
 
   useEffect(() => {
+    const empleados = location.state?.empleadoEdit;
+    if(empleados) {
+      setIsEditing(true);
+      setEmpleadoEdit(empleados);
+    }
    getEmpleados();
     getAreas();
     getRoles();
-  }, []);
+  }, [location.state]);
 
   const areaNameMap = areas.reduce((acc, area) => {
     acc[area.id] = area.nombre;
@@ -32,6 +40,11 @@ const Empleados = () => {
   const handleQuiereBoletinChange = (event) => {
     setQuiereBoletin(event.target.checked);
   };
+
+  const handleEditEmpleado = (empleados) => {
+    setIsEditing(true);
+    setEmpleadoEdit(empleados);
+  }
 
   const handleDeleteEmpleado = (empleadoId) => {
     deleteEmpleado(empleadoId);
@@ -46,6 +59,15 @@ const Empleados = () => {
   };
 
   const onSubmit = handleSubmit((data)=> {
+    if(isEditing){
+      updateEmpleado(empleadoEdit._id, data)
+    }else{
+      createEmpleados(data)
+    }
+
+    setIsEditing(false);
+    setEmpleadoEdit(null);
+
     const empleadoData = {
       nombre: data.nombre,
       email: data.email,
@@ -65,28 +87,28 @@ const Empleados = () => {
       </div>
           <div className='nombre'>
             <label htmlFor="Nombre">Nombre Completo * </label>
-            <input type="text" id='Nombre' className='nombre-input' {...register("nombre")} placeholder='Nombre completo del empleado' />
+            <input type="text" id='Nombre' className='nombre-input' {...register("nombre")} defaultValue={empleadoEdit?.nombre || ""} placeholder='Nombre completo del empleado' />
           </div>
           <div className='correo'>
             <label htmlFor="Correo">Correo electronico * </label>
-            <input type="text" id='correo' className='correo-input' {...register("email")} placeholder='Correo electronico' />
+            <input type="text" id='correo' className='correo-input' {...register("email")} defaultValue={empleadoEdit?.email || ""} placeholder='Correo electronico' />
           </div>
           <div className="flex">
             <label className="sexo">Sexo * </label>
             <div className="flex flex-col">
               <label htmlFor="masculino">Masculino 
-                <input type="radio" id="masculino" className='mascu-input' {...register("sexo")} value="M" />
+                <input type="radio" id="masculino" className='mascu-input' {...register("sexo")} defaultValue={empleadoEdit?.sexo || ""} />
               </label>
               <label htmlFor="femenino">Femenino 
-                <input type="radio" id="femenino" className='mascu-input' {...register("sexo")} value="F"/>
+                <input type="radio" id="femenino" className='mascu-input' {...register("sexo")} defaultValue={empleadoEdit?.sexo || ""} />
               </label>
             </div>
           </div>
           <div>
             <label htmlFor="area" className='area'>Area * </label>
-            <select name='areas' id='areas' className='areas' {...register("area_id")}>
+            <select name='areas' id='areas' className='areas' {...register("area_id")} defaultValue={empleadoEdit?.area_id || ""}>
               {areas.map((area)=>(
-                <option key={area.id} value={area.id}>
+                <option key={area.id} >
                   {area.nombre}
                 </option>
               ))}
@@ -95,9 +117,9 @@ const Empleados = () => {
           <div className='flex'>
             <label htmlFor="descripcion" className='descripcion'>Descripción * </label>
               <div className='flex flex-col'>
-                <textarea name="" id="" cols="" rows="" className="descripcion-tex" placeholder='Descripción de la experiencia del empleado' {...register("descripcion")}></textarea>
+                <textarea name="" id="" cols="" rows="" className="descripcion-tex" placeholder='Descripción de la experiencia del empleado' {...register("descripcion")} defaultValue={empleadoEdit?.descripcion || ""}></textarea>
                 <div className='flex'>
-                  <input type="checkbox" id='boletin' className='boletin-input' onChange={handleQuiereBoletinChange} />
+                  <input type="checkbox" id='boletin' className='boletin-input' onChange={handleQuiereBoletinChange} defaultValue={empleadoEdit?.boletin || ""} />
                   <label htmlFor="boletin" className='boletin'>Desea recibir el boletin informativo</label>
                 </div>
               </div>
@@ -109,34 +131,50 @@ const Empleados = () => {
                   <div key={role.id} className='rol' >
                       <input type="checkbox" name="" id="" className='rol-input' checked={rolesSeleccionados && rolesSeleccionados.includes(role.id)} onChange={() => handleRolchange(role.id)}/>
                       {role.nombre}
-                    <input type="hidden" name="roles" value={role.id} {...register(`boletin[${role.id}]`)} />
+                    <input type="hidden" name="roles" {...register(`boletin[${role.id}]`)} defaultValue={empleadoEdit?.boletin || ""} />
                   </div>
                 ))}
                 </div>
           </div>
-          <button className='buton-guardadr'>Guardar</button>
+          <button className='buton-guardadr'>{isEditing ? "Actualizar" : "Guardar"}</button>
       </form>
       <table className='tabla'>
-      <tr className='emple'>
-          <th><div className='icono'><AiOutlineUser/>Nombre</div></th>
-          <th><div className='icono'><MdOutlineAlternateEmail/>Email</div></th>
-          <th><div className='icono'><BsGenderAmbiguous/>Sexo</div></th>
-          <th><div className='icono'><FaBriefcase/>Area</div></th>
-          <th><div className='icono'><MdOutlineEmail/>Boletin</div></th>
-          <th >Modificar</th>
-          <th>Eliminar</th>
-      </tr>
-        {empleados.map((empleado, index) => (
-          <tr key={empleado.id} className={index % 2 === 0 ? 'fila-par' : 'fila-impar'}>
-            <td>{empleado.nombre}</td>
-            <td>{empleado.email}</td>
-            <td>{empleado.sexo}</td>
-            <td>{areaNameMap[empleado.area_id]}</td>
-            <td>{empleado.boletin}</td>
-            <td><button><div><BiEdit /></div></button></td>
-            <td><button><div><MdDelete className='delete' onClick={() => handleDeleteEmpleado(empleado.id)} /></div></button></td>
+        <thead>
+          <tr className='emple'>
+            <th><div className='icono'><AiOutlineUser/>Nombre</div></th>
+            <th><div className='icono'><MdOutlineAlternateEmail/>Email</div></th>
+            <th><div className='icono'><BsGenderAmbiguous/>Sexo</div></th>
+            <th><div className='icono'><FaBriefcase/>Area</div></th>
+            <th><div className='icono'><MdOutlineEmail/>Boletin</div></th>
+            <th>Modificar</th>
+            <th>Eliminar</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {empleados.map((empleado, index) => (
+            <tr key={empleado.id} className={index % 2 === 0 ? 'fila-par' : 'fila-impar'}>
+              <td>{empleado.nombre}</td>
+              <td>{empleado.email}</td>
+              <td>{empleado.sexo}</td>
+              <td>{areaNameMap[empleado.area_id]}</td>
+              <td>{empleado.boletin}</td>
+              <td>
+                <button>
+                  <div>
+                    <BiEdit onClick={() => handleEditEmpleado(empleado)}/>
+                  </div>
+                </button>
+              </td>
+              <td>
+                <button>
+                  <div>
+                    <MdDelete onClick={() => handleDeleteEmpleado(empleado.id)} />
+                  </div>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- server/src/routes/empleados.routes.js
+++ server/src/routes/empleados.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createEmpleado, getEmpleados, getEmpleadoId, updateEmpleado, deleteEmpleado} from "../controllers/empleadosControllers.js";
-// import { validateSchema } from "../middleware/validator.middleware.js";
-// import { crearEmpleado } from "../schema/empleados.schema.js";
+import { validateSchema } from "../middleware/validator.middleware.js";
+import { crearEmpleado } from "../schema/empleados.schema.js";
 
 const router = Router();
 
 router.get('/empleado',getEmpleados);
 router.get('/empleados/:id',getEmpleadoId);
-router.post('/empleados', createEmpleado);
+router.post('/empleados',validateSchema(crearEmpleado), createEmpleado);
 router.put('/empleados/:id',updateEmpleado);
 router.delete('/empleados/:id',deleteEmpleado);
 
